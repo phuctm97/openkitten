d04bc3393f987597da602fc8ebdc1587731d0c10
Minh-Phuc Tran <phuctm97@gmail.com>
2026-04-13 14:16:09 +0700

    Update server greeting text

--- packages/server/lib/main.ts
+++ packages/server/lib/main.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 
 const hono = new Hono();
 
-hono.get("/", (context) => context.text("hello world"));
+hono.get("/", (context) => context.text("Hello, world!"));
 
 export default {
   fetch: hono.fetch,
--- packages/server/test/hello-world-server.test.ts
+++ packages/server/test/hello-world-server.test.ts
@@ -7,6 +7,6 @@ describe("server", () => {
     const response = await server.fetch(new Request("http://localhost/"));
 
     expect(response.status).toBe(200);
-    await expect(response.text()).resolves.toBe("hello world");
+    await expect(response.text()).resolves.toBe("Hello, world!");
   });
 });
