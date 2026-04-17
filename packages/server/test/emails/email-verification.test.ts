b7f2138dd0be293e64bbd945f86c573a4752df2a
Minh-Phuc Tran <phuctm97@gmail.com>
2026-04-17 12:19:17 +0700

    Update dependencies to latest versions

--- packages/server/test/emails/email-verification.test.ts
+++ packages/server/test/emails/email-verification.test.ts
@@ -1,4 +1,4 @@
-import { render } from "@react-email/components";
+import { render } from "react-email";
 import { expect, it } from "vitest";
 import EmailVerification from "~/lib/emails/email-verification";
 import { websiteURL } from "~/lib/website-url";
