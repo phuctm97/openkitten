f90a6a27b9361c9ba96f4897297abbd78be4d2c9
Minh-Phuc Tran <phuctm97@gmail.com>
2026-04-17 13:20:41 +0700

    Fix world navigator type filename

--- packages/world/lib/navigator-atom.ts
+++ packages/world/lib/navigator-atom.ts
@@ -1,4 +1,4 @@
 import { atomWithPending } from "~/lib/atom-with-pending";
-import type { Navigator } from "~/lib/nativator";
+import type { Navigator } from "~/lib/navigator";
 
 export const navigatorAtom = atomWithPending<Navigator>();
