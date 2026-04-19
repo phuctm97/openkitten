118371c877f9bada34b9d48c87e9202d0b67328e
Minh-Phuc Tran <phuctm97@gmail.com>
2026-04-19 15:27:55 +0700

    Use bun --bun for generate commands (#232)

--- prepare.ts
+++ prepare.ts
@@ -11,17 +11,17 @@ const promises: Promise<unknown>[] = [];
 
 for await (const reactRouterConfigFile of reactRouterConfigFiles.scan(".")) {
   const dir = dirname(reactRouterConfigFile);
-  promises.push(Bun.$`bun react-router typegen`.cwd(dir).quiet());
+  promises.push(Bun.$`bun --bun react-router typegen`.cwd(dir).quiet());
 }
 
 for await (const nextConfigFile of nextConfigFiles.scan(".")) {
   const dir = dirname(nextConfigFile);
-  promises.push(Bun.$`bun next typegen`.cwd(dir).quiet());
+  promises.push(Bun.$`bun --bun next typegen`.cwd(dir).quiet());
 }
 
 for await (const fumadocsConfigFile of fumadocsConfigFiles.scan(".")) {
   const dir = dirname(fumadocsConfigFile);
-  promises.push(Bun.$`bun fumadocs-mdx`.cwd(dir).quiet());
+  promises.push(Bun.$`bun --bun fumadocs-mdx`.cwd(dir).quiet());
 }
 
 await Promise.all(promises);
